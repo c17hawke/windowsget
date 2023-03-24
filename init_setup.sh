8642fedb2cf98b923fa2ac14deaadb68e034156f
c17hawke <sunny.c17hawke@gmail.com>
2023-03-24 18:01:08 +0530

    skeleton complete

--- init_setup.sh
+++ init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START"
-echo [$(date)]: "Creating conda env with python <PYTHON_VERSION>"
-conda create --prefix ./env python=<PYTHON_VERSION> -y
+echo [$(date)]: "Creating conda env with python 3.8"
+conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate env"
 source activate ./env
 echo [$(date)]: "intalling dev requirements"
